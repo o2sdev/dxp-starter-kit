f5cfedc544dbbd791d020bb18b7c996531d5d781
Marcin Krasowski <marcin.krasowski@hycom.pl>
2025-08-20 11:31:42 +0200

    feat: added CLI script to bootstrap the project

---
+++ packages/cli/create-dxp-app/.prettierrc.mjs
@@ -0,0 +1,11 @@
+import baseConfig from '@dxp/prettier-config/base.mjs';
+
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+    ...baseConfig,
+};
+
+export default config;
