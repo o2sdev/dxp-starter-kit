49d2bb0955233e5792068a4639f2d4dd9cbd73bb
Marcin Krasowski <marcin.krasowski@hycom.pl>
2025-09-17 12:40:33 +0200

    feat: reduced JS bundle size by not moving to dynamic icon loading

---
+++ .changeset/sour-worlds-wait.md
@@ -0,0 +1,6 @@
+---
+'@dxp/frontend': patch
+'@dxp/ui': patch
+---
+
+reduced JS bundle size by not moving to dynamic icon loading
--- packages/ui/src/lib/utils.ts
+++ packages/ui/src/lib/utils.ts
@@ -7,7 +7,7 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export const baseVariant = cva(
-    'inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-hidden focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0',
+    'inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-hidden focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
     {
         variants: {
             variant: {
